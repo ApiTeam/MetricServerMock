29e091528233897597d113c31689e44263329aaf
Andrey Murzhuhin <andrey.murzhuhin@api.nnov.ru>
2021-05-28 11:59:20 +0300

    flat data

--- routes/data.js
+++ routes/data.js
@@ -57,4 +57,38 @@ router.get("/data/:section", (req, res, next) => {
 
 })
 
+router.get("/data/flat/:section", (req, res, next) => {
+    const {section} = req.params
+    const {year, quarter} = req.query
+
+    if (!data[section]) {
+        next(createError(404, 'Секция справочника не найдена'));
+    }
+    if (Number.isNaN(year) || year <= 1900 || year > 2021) {
+        next(createError(400, 'Год - это число в промежутке от 1900 до 2021'));
+    }
+    if (Number.isNaN(quarter) || quarter < 1 || year > 4) {
+        next(createError(400, 'Квартал принимает следующие значения: 1,2,3,4'));
+    }
+
+    const seededRandom = new SeededRandom(Number(year + quarter))
+
+    const result = [];
+    const sectionDict = data[section];
+
+    Object.keys(sectionDict).forEach((keyMain) => {
+        Object.keys(sectionDict[keyMain]).forEach((keySub) => {            
+            quarters[quarter - 1].forEach(value => {                
+                result.push({
+                    mainId : keyMain,
+                    subId: keySub,
+                    period: value + ' ' + year ,
+                    costs: seededRandom.nextRange(0, year)
+                })
+            })            
+        })
+    })
+    res.json(result)
+})
+
 module.exports = router;
